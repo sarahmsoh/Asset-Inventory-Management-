e79de9b5bfea0a626da79a62084ce2530695dc69
sarahmsoh <sarahmuriithi523@gmail.com>
2025-02-23 20:56:59 +0300

    AIM-6 initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import DashboardComponent from './EmployeeDashboard.js';
+import RequestFormComponent from './RequestForm.js';
+
+// import EmployeeDashboard from './EmployeeDashboard';
+// import RequestFormComponent from './RequestForm';
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<DashboardComponent />} />
+        <Route path="/request" element={<RequestFormComponent />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
